2b0cc20fbe8cf5ceb8f89c5de610d68dcb57ec5e
tarako25 <takenoko1603@gmail.com>
2023-08-12 11:01:19 +0900

    引継ぎ

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,86 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mysql from 'mysql';
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'POST') {
+    const I_data = req.body.Insert_data;
+    if (I_data !== '') {
+      console.log(I_data);
+      const connection = Connectsql();
+      InsertSQL(connection, I_data);
+      connection.end((err) => {
+        if (err) {
+          console.error('MySQL切断エラー:', err);
+          return;
+        }
+        console.log('MySQL接続を切断しました。');
+      });
+      res.status(200).send('POST通信成功');
+    } else {
+      console.log('入力値が空です');
+      res.status(400).send('入力値が空です');
+    }
+  }
+  if (req.method === 'GET') {
+    console.log('GET通信成功');
+    const connection = Connectsql();
+    SelectSQL(connection, (err, result) => {
+      connection.end();
+      console.log("MySQLの接続を終了しました。")
+      if (err) {
+        console.error('Select文エラー:', err);
+        res.status(500).json({ error: 'データの取得に失敗しました。' });
+        return;
+      }
+    });
+    res.status(200).json(result);
+  }
+}
+
+// MySQL接続
+function Connectsql() {
+  // MySQL接続情報
+  const connection = mysql.createConnection({
+    host: 'localhost',      // データベースホスト名
+    user: 'root',           // ユーザー名 (デフォルトは'root')
+    password: '',           // パスワード
+    database: 'next_app'
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('MySQL接続エラー:', err);
+      return;
+    }
+    console.log('MySQLに接続しました。');
+  });
+
+  return connection;
+}
+
+// 実行クエリ(INSERT)
+function InsertSQL(connection, data) {
+  const sqlQuery = 'INSERT INTO next_table (msg) values (?)';
+  connection.query(sqlQuery, [data], (err, result) => {
+    if (err) {
+      console.error('INSERT文エラー:', err);
+      return;
+    } else {
+      console.log('新しいレコードが追加されました。',data);
+    }
+  });
+}
+
+// 実行クエリ(SELECT)
+function SelectSQL(connection, callback) {
+  const sqlQuery = 'SELECT * FROM next_table';
+  connection.query(sqlQuery, (err, result) => {
+    if (err) {
+      console.error('Select文エラー:', err);
+      callback(err, null);
+      return;
+    } else {
+      callback(null, result);
+    }
+  });
 }
--- src/pages/conponents/Main/Main.jsx
+++ src/pages/conponents/Main/Main.jsx
@@ -1,29 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Main.module.css'
 import noimg from 'public/imgs/noimg.png' 
 import Image from 'next/image'
 
 function Main() {
+  
+  //ロード時の表示 
+  const fetchLorad = async() => {
+    const response = await fetch('api/hello');
+    console.log("ざっこ");
+    if(!response.ok){
+      console.log("ロード中にエラーが出ました");
+    }
+    const data = await response.json();
+    console.log('全てのレコードを表示', data);
+  }
+
+  useEffect(() => {
+    fetchLorad();
+  }, []); 
+
+  //送信
+  const [value, setValue] = useState('');
+  const fetchData = async(e, Insert_data) => {
+    e.preventDefault();
+    const requestBody = {};
+    requestBody[Insert_data] = value;
+
+    await fetch('api/hello',{
+      method:'POST',
+      headers: {
+        'Content-type':'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+    setValue('');
+  }
+  const enterValue = (e) =>{
+    setValue(e.target.value);
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.mainbox}>
-
         {/* 投稿(入力)) */}
-        <form className={styles.postbox}>
+        <div className={styles.postbox}>
           <div className={styles.profile_box}>
-            <Image src={noimg} className={styles.profile_img}/>
+            <Image src={noimg} className={styles.profile_img} alt="アイコン"/>
             <div className={styles.profile_name}>Lorem</div>
           </div>
-          <input type="text" className={styles.post_msg}/>
+          <input type="text" placeholder="今日の出来事を投稿しよう!" className={styles.post_msg} value={value} onChange={enterValue}/>
           <div className={styles.reaction}>
-            <button className={styles.button}>送信</button>
+            <button className={styles.button} onClick={(e) => fetchData(e, 'Insert_data')}>送信</button>
           </div>
-        </form>
+        </div>
 
         {/* 投稿*/}
         <div className={styles.postbox}>
           <div className={styles.profile_box}>
-            <Image src={noimg} className={styles.profile_img}/>
+            <Image src={noimg} className={styles.profile_img} alt="アイコン"/>
             <div className={styles.profile_name}>Lorem</div>
           </div>
           <div className={styles.post_msg}>
